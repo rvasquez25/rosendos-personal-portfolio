0a48c0559f0797705051b4124fdf83ded0f63062
LAPTOP-3USP6FRD\rosen <rvasquez25@miners.utep.edu>
2022-07-24 22:57:49 -0600

    created experience page

--- rosendos-personal-portfolio/src/pages/Experience.js
+++ rosendos-personal-portfolio/src/pages/Experience.js
@@ -5,7 +5,8 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
+import { faGraduationCap, faHeadset, faBook, faRocket } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Experience.css";
 
 function Experience() {
   return (
@@ -13,12 +14,42 @@ function Experience() {
       <VerticalTimeline lineColor="#3e497a">
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2010 - 2014"
-          iconStyle={{ background: "#3e497a", colo: "#fff" }}
+          date="2015 - 2019"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
           icon={<FontAwesomeIcon icon={faGraduationCap} />}
         >
-          <h3 className="vertical-timeline-element-title">My Random High School, Random Place, Random State</h3>
+          <h3 className="vertical-timeline-element-title">Pebble Hills High School, El Paso, TX</h3>
           <p>High School Diploma</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019 - 2021"
+          iconStyle={{ background: "#cf3047", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faHeadset} />}
+        >
+          <h3 className="vertical-timeline-element-title">Technology Suppport at The University of Texas at El Paso</h3>
+          <p>Addressing issues daily via phone, email, or chat regarding password resets, account activations, software 
+            installations, remote sessions, and creating requests for documentation purposes. As well as providing 
+            customer service with patience and kindness and maintaining a clean workspace when working on campus
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019 - Present"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faBook} />}
+        >
+          <h3 className="vertical-timeline-element-title">The University of Texas at El Paso, El Paso, TX</h3>
+          <p>Undergraduate, Computer Science, SENIOR</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - Present"
+          iconStyle={{ background: "#cf3047", color: "#fff" }}
+          icon={<FontAwesomeIcon icon={faRocket} />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Developer at SPS ARS Aerial Remote Sensing</h3>
+          <p>Advanced unmanned aircraft and operations</p>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
