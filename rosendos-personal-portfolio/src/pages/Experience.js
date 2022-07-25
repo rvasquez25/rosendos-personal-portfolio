c86700a7060fa20ce611c3456d2eaf4c486a54aa
LAPTOP-3USP6FRD\rosen <rvasquez25@miners.utep.edu>
2022-07-24 21:54:24 -0600

    pushing to make sure it works

--- rosendos-personal-portfolio/src/pages/Experience.js
+++ rosendos-personal-portfolio/src/pages/Experience.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 
 function Experience() {
-  return ( 
-    <div>Experience</div>
-  )
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2010 - 2014"
+          iconStyle={{ background: "#3e497a", colo: "#fff" }}
+          icon={<FontAwesomeIcon icon={faGraduationCap} />}
+        >
+          <h3 className="vertical-timeline-element-title">My Random High School, Random Place, Random State</h3>
+          <p>High School Diploma</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
-export default Experience
+export default Experience;
