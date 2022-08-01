3a1d6e55422c7cd1ac7641f5738111f4ffff7fa8
LAPTOP-3USP6FRD\rosen <rvasquez25@miners.utep.edu>
2022-08-01 01:41:05 -0600

    re-created skills section

--- rosendos-personal-portfolio/src/pages/Home.js
+++ rosendos-personal-portfolio/src/pages/Home.js
@@ -36,8 +36,8 @@ function Home() {
       {/* </div> */}
       <div className="skills">
         <h1> Skills</h1>
-        <div class="about-content">
-          <ol className="list">
+        <div class="skills-content">
+          {/* <ol className="list">
             <li className="item">
               <h2>Front-end</h2>
               <span>ReactJS, HTML, CSS, NPM</span>
@@ -50,7 +50,56 @@ function Home() {
               <h2>Languages</h2>
               <span>Javascript, Java, Python, C#, C, C++</span>
             </li>
-          </ol>
+          </ol> */}
+          <div className="column left">
+            <div className="text">My Experience.</div>
+            <p>Python, C, and Java are my stronger languages because I began learning these languages in 2019(freshman year of college). I've completed Intro to Computer Science, Elem. Data Struct./Algorithms, and Data Structures in Java and Python and I as well understand how to implement Data Structure level concepts in C. I researched HTML, CSS, and JavaScript on my own time freshman year but because of my internship, I've been able to practice these languages and I am continously learning on my own as well.</p>
+            <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">Read more</a>
+          </div>
+          <div className="column right">
+            <div className="bars">
+              <div className="info">
+                <span>HTML</span>
+                <span>Beginner</span>
+              </div>
+              <div class="line html"></div>
+            </div>
+            <div className="bars">
+              <div className="info">
+                <span>CSS</span>
+                <span>Beginner</span>
+              </div>
+              <div class="line css"></div>
+            </div>
+            <div className="bars">
+              <div className="info">
+                <span>Javascript</span>
+                <span>Beginner</span>
+              </div>
+              <div class="line js"></div>
+            </div>
+            <div className="bars">
+              <div className="info">
+                <span>Python</span>
+                <span>Intermediate</span>
+              </div>
+              <div class="line python"></div>
+            </div>
+            <div className="bars">
+              <div className="info">
+                <span>C</span>
+                <span>Intermediate</span>
+              </div>
+              <div class="line c"></div>
+            </div>
+            <div className="bars">
+              <div className="info">
+                <span>Java</span>
+                <span>Intermediate</span>
+              </div>
+              <div class="line java"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
