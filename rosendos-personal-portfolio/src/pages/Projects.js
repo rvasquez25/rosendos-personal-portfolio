03abddf83db234e89305d07aed1fa4337b0749e2
rvasquez25 <rosendovasquez915@gmail.com>
2022-08-12 15:51:36 -0600

    forgot to remove pieces of future projects from projects

--- rosendos-personal-portfolio/src/pages/Projects.js
+++ rosendos-personal-portfolio/src/pages/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import ProjectItemFuture from "../components/ProjectItemFuture"
 import { ProjectList } from "../helpers/ProjectList";
-import { ProjectInProgress } from "../helpers/ProjectInProgress";
 import "../styles/Project.css";
 // import video2 from "../assets/homeVideo4.mp4";
 
@@ -19,12 +17,6 @@ function Projects() {
           return <ProjectItem id={idx} name={project.name} image={project.image} />;
         })}
       </div>
-      {/* <h1>Projects in Progress</h1>
-      <div className="projectList">
-        {ProjectInProgress.map((project, idx) => {
-          return <ProjectItemFuture id={idx} name={project.name} image={project.image} />;
-        })}
-      </div> */}
     </div>
   );
 }
